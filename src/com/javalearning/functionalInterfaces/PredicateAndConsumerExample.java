f949a210ef4c5a8b6599c273aa24785579cc975b
Murillo Cear <murilloc@gmail.com>
2019-12-14 22:45:12 -0300

    initial commit Lambda Consumer interface BiConsumer functional Interfaces Predicate Predicate And Consumer BiPredicate and BiConsumer

--- src/com/javalearning/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/com/javalearning/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.javalearning.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,10 +13,18 @@ public class PredicateAndConsumerExample {
 
     private static Predicate<Student> studentGradeLevelPredicate = (student) -> student.getGradeLevel() >= 3;
     private static Predicate<Student> studentGpaPredicate = (student) -> student.getGpa() >= 3.9;
+
+    private static BiPredicate<Integer, Double> studentBiPredicate = (gradeLevel, gpa) -> gradeLevel >= 3 && gpa >= 3.9;
     private static BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 
     private static Consumer<Student> studentConsumer = (student -> {
         if (studentGradeLevelPredicate.and(studentGpaPredicate).test(student)) {
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+    });
+
+    private static Consumer<Student> studentBiPredicateConsumer = (student -> {
+        if (studentBiPredicate.test(student.getGradeLevel(), student.getGpa())) {
             studentBiConsumer.accept(student.getName(), student.getActivities());
         }
     });
@@ -26,9 +35,16 @@ public class PredicateAndConsumerExample {
         studentList.forEach(studentConsumer);
     }
 
+    public static void printNameAndActivitiesBiPredicate(List<Student> studentList) {
+        studentList.forEach(studentBiPredicateConsumer);
+    }
+
     public static void main(String[] args) {
 
         List<Student> studentList = StudentDataBase.getAllStudents();
+        System.out.println("---------- Consumer and Predicate-------------");
         printNameAndActivities(studentList);
+        System.out.println("-----------BiConsumer and BiPredicate---------");
+        printNameAndActivitiesBiPredicate(studentList);
     }
 }
